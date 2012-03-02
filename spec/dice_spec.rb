d73b60adc37214d4097385f6ddfeb22b73508d77
Denis <de.tierno@gmail.com>
2012-03-02 16:23:55 -0300

    improved tests

--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
+
 describe Dice do
+  NTIMES = 300
   
   describe "new with default values" do
     
@@ -8,7 +10,7 @@ describe Dice do
       let(:dice) { Dice.new }
 
       it "return value between 1 to 6" do
-        1.upto(6).include?(dice.roll).should be_true
+        NTIMES.times { 1.upto(6).include?(dice.roll).should be_true }
       end
     end
     
@@ -16,19 +18,19 @@ describe Dice do
       let(:dice) { Dice.new }
 
       it "return value between 2 to 12" do
-        2.upto(12).include?(dice.roll_two).should be_true
+        NTIMES.times { 2.upto(12).include?(dice.roll_two).should be_true }
       end
 
       it "cant return 1" do
-        30.times { 2.upto(12).include?(dice.roll_two).should_not == 1 }
+        NTIMES.times { 2.upto(12).include?(dice.roll_two).should_not == 1 }
       end
 
       it "cant be lesser then 2" do
-        30.times { dice.roll_two.should_not < 2 }
+        NTIMES.times { dice.roll_two.should_not < 2 }
       end
 
       it "cant return 13" do
-        30.times { dice.roll_two.should_not > 13 }
+        NTIMES.times { dice.roll_two.should_not > 13 }
       end
     end
     
@@ -36,11 +38,11 @@ describe Dice do
       let(:dice) { Dice.new }
 
       it "return value between 1 to 20" do
-        30.times { 1.upto(20).include?(dice.roll_d20).should be_true }
+        NTIMES.times { 1.upto(20).include?(dice.roll_d20).should be_true }
       end
       
       it "cant return 21" do
-        30.times { dice.roll_two.should_not > 20 }
+        NTIMES.times { dice.roll_two.should_not > 20 }
       end
     end
     
@@ -48,15 +50,75 @@ describe Dice do
       let(:dice) { Dice.new }
 
       it "return value between 1 to 10" do
-        30.times { 2.upto(20).include?(dice.roll_two_d10).should be_true }
+        NTIMES.times { 2.upto(20).include?(dice.roll_two_d10).should be_true }
       end
       
       it "can be less than 2" do
-        30.times { dice.roll_two_d10.should_not < 2 }
+        NTIMES.times { dice.roll_two_d10.should_not < 2 }
       end
       
       it "cant return 21" do
-        30.times { dice.roll_two.should_not > 20 }
+        NTIMES.times { dice.roll_two_d10.should_not > 20 }
+      end
+    end
+     
+  end
+
+  describe "new with 12 faces and 3 dices" do
+    
+    describe ".roll" do
+      let(:dice) { Dice.new(12,3) }
+
+      it "return value between 3 to 36" do
+        3.upto(36).include?(dice.roll).should be_true
+      end
+    end
+    
+    describe ".roll_two" do
+      let(:dice) { Dice.new(12,3) }
+
+      it "return value between 2 to 24" do
+        NTIMES.times { 2.upto(24).include?(dice.roll_two).should be_true }
+      end
+
+      it "cant return 1" do
+        NTIMES.times { 2.upto(24).include?(dice.roll_two).should_not == 1 }
+      end
+
+      it "cant be lesser then 2" do
+        NTIMES.times { dice.roll_two.should_not < 2 }
+      end
+
+      it "cant return 25" do
+        NTIMES.times { dice.roll_two.should_not > 25 }
+      end
+    end
+    
+    describe ".roll_d20" do
+      let(:dice) { Dice.new(12,3) }
+
+      it "return value between 1 to 60" do
+        NTIMES.times { 3.upto(60).include?(dice.roll_d20).should be_true }
+      end
+      
+      it "cant return 61" do
+        NTIMES.times { dice.roll_two.should_not > 61 }
+      end
+    end
+    
+    describe ".roll_two_d10" do
+      let(:dice) { Dice.new(12,3) }
+
+      it "return value between 1 to 10" do
+        NTIMES.times { 2.upto(20).include?(dice.roll_two_d10).should be_true }
+      end
+      
+      it "can be less than 2" do
+        NTIMES.times { dice.roll_two_d10.should_not < 2 }
+      end
+      
+      it "cant return 21" do
+        NTIMES.times { dice.roll_two_d10.should_not > 20 }
       end
     end
      
